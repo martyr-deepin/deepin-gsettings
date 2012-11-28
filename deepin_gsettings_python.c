9c4952952ea1a6ae2ed553828d8268d1334db4db
xiangzhai <xiangzhai83@gmail.com>
2012-11-28 18:31:27 +0800

    Init python binding template

---
+++ deepin_gsettings_python.c
@@ -0,0 +1,185 @@
+/* 
+ * Copyright (C) 2012 Deepin, Inc.
+ *               2012 Zhai Xiang
+ *
+ * Author:     Zhai Xiang <zhaixiang@linuxdeepin.com>
+ * Maintainer: Zhai Xiang <zhaixiang@linuxdeepin.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <Python.h>
+#include <gio/gio.h>
+
+/* Safe XDECREF for object states that handles nested deallocations */
+#define ZAP(v) do {\
+    PyObject *tmp = (PyObject *)(v); \
+    (v) = NULL; \
+    Py_XDECREF(tmp); \
+} while (0)
+
+typedef struct {
+    PyObject_HEAD
+    PyObject *dict; /* Python attributes dictionary */
+    GSettings *handle;
+} DeepinGSettingsObject;
+
+static PyObject *deepin_gsettings_object_constants = NULL;
+static PyTypeObject *m_DeepinGSettings_Type = NULL;
+
+static DeepinGSettingsObject *m_init_deepin_gsettings_object();
+static DeepinGSettingsObject *m_new(PyObject *self, PyObject *args);
+
+static PyMethodDef deepin_gsettings_methods[] = 
+{
+    {"new", m_new, METH_VARARGS, "Deepin GSettings Construction"}, 
+    {NULL, NULL, 0, NULL}
+};
+
+static char m_get_value_doc[] = "Gets the value that is stored at key in settings";
+static char m_set_value_doc[] = "Sets key in settings to value";
+
+static PyObject *m_delete(DeepinGSettingsObject *self);
+static PyObject *m_get_boolean(DeepinGSettingsObject *self);
+static PyObject *m_set_boolean(DeepinGSettingsObject *self, PyObject *args);
+static PyObject *m_get_int(DeepinGSettingsObject *self);
+static PyObject *m_set_int(DeepinGSettingsObject *self, PyObject *args);
+static PyObject *m_get_uint(DeepinGSettingsObject *self);
+static PyObject *m_set_uint(DeepinGSettingsObject *self, PyObject *args);
+static PyObject *m_get_double(DeepinGSettingsObject *self);
+static PyObject *m_set_double(DeepinGSettingsObject *self, PyObject *args);
+static PyObject *m_get_strv(DeepinGSettingsObject *self, PyObject *args);
+static PyObject *m_set_strv(DeepinGSettingsObject *self, PyObject *args);
+
+static PyMethodDef deepin_gsettings_object_methods[] = 
+{
+    {"delete", m_delete, METH_NOARGS, "Deepin GSettings Object Destruction"}, 
+    {"get_boolean", m_get_boolean, METH_NOARGS, m_get_value_doc}, 
+    {"set_boolean", m_set_boolean, METH_VARARGS, m_set_value_doc}, 
+    {"get_int", m_get_int, METH_NOARGS, m_get_value_doc}, 
+    {"set_int", m_set_int, METH_VARARGS, m_set_value_doc}, 
+    {"get_uint", m_get_uint, METH_NOARGS, m_get_value_doc}, 
+    {"set_uint", m_set_uint, METH_VARARGS, m_set_value_doc}, 
+    {"get_double", m_get_double, METH_NOARGS, m_get_value_doc}, 
+    {"set_double", m_set_double, METH_VARARGS, m_set_value_doc}, 
+    {"get_strv", m_get_strv, METH_NOARGS, m_get_value_doc}, 
+    {"set_strv", m_set_strv, METH_VARARGS, m_set_value_doc}, 
+    {NULL, NULL, 0, NULL}
+};
+
+static void m_deepin_gsettings_dealloc(DeepinGSettingsObject *self) 
+{
+    PyObject_GC_UnTrack(self);
+    Py_TRASHCAN_SAFE_BEGIN(self)
+
+    ZAP(self->dict);
+    m_delete(self);
+
+    PyObject_GC_Del(self);
+    Py_TRASHCAN_SAFE_END(self)
+}
+
+static PyObject *m_deepin_gsettings_getattr(DeepinGSettingsObject *dgo, char *name) 
+{
+    return m_getattr((PyObject *)dgo, name, dgo->dict, deeping_gsettings_object_constants, deepin_gsettings_object_methods);
+}
+
+static PyObject *m_deepin_gsettings_setattr(DeepinGSettingsObject *dgo, char *name, PyObject *v) 
+{
+    return m_setattr(&ao->dict, name, v);
+}
+
+static PyTypeObject DeepinGSettings_Type = {
+    PyObject_HEAD_INIT(NULL)
+    0, 
+    "DeepinGSettings.new", 
+    sizeof(DeepinGSettingsObject), 
+    0, 
+    (destructor)m_deepin_gsettings_dealloc,
+    0, 
+    (getattrfunc)m_deepin_gsettings_getattr, 
+    (setattrfunc)m_deepin_gsettings_setattr, 
+    0, 
+    0, 
+    0,  
+    0,  
+    0,  
+    0,  
+    0,  
+    0,  
+    0,  
+    0,  
+    Py_TPFLAGS_HAVE_GC,
+    0,  
+    (traverseproc)m_deepin_gsettings_traverse, 
+    (inquiry)m_deepin_gsettings_clear
+};
+
+PyMODINIT_FUNC initdeepin_gsettings() 
+{
+    PyObject *m = NULL;
+             
+    m_DeepinGSettings_Type = &DeepinGSettings_Type;
+    DeepinGSettings_Type.ob_type = &PyType_Type;
+
+    m = Py_InitModule("deepin_gsettings", deepin_gsettings_methods);
+    if (!m)
+        return;
+
+    deepin_gsettings_object_constants = PyDict_New();
+}
+
+static DeepinGSettingsObject *m_init_deepin_gsettings_object() 
+{
+    DeepinGSettingsObject *self = NULL;
+
+    self = (DeepinGSettingsObject *) PyObject_GC_New(DeepinGSettingsObject, m_DeepinGSettings_Type);
+    if (!self)
+        return NULL;
+    PyObject_GC_Track(self);
+
+    self->dict = NULL;
+    self->handle = NULL;
+
+    return self;
+}
+
+static DeepinGSettingsObject *m_new(PyObject *dummy, PyObject *args) 
+{
+    DeepinGSettingsObject *self = NULL;
+    char *schema_id = NULL;
+    
+    self = m_init_deepin_gsettings_object();
+    if (!self)
+        return NULL;
+
+    if (!PyArg_ParseTuple(args, "s", &schema_id))
+        return NULL;
+
+    self->handle = g_settings_new(schema_id);
+
+    return self;
+}
+
+static PyObject *m_delete(DeepinGSettingsObject *self) 
+{
+    if (self->handle) {
+        g_object_unref(self->handle);
+        self->handle = NULL;
+    }
+
+    Py_INCREF(Py_None);
+    return Py_None;
+}
--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ def pkg_config_cflags(pkgs):
     '''List all include paths that output by `pkg-config --cflags pkgs`'''
     return map(lambda path: path[2::], commands.getoutput('pkg-config --cflags-only-I %s' % (' '.join(pkgs))).split())
 
-deepin_gsettings_mod = Extension('deepin-gsettings', 
+deepin_gsettings_mod = Extension('deepin_gsettings', 
                 include_dirs = pkg_config_cflags(['gio-2.0']),
                 libraries = ['gio-2.0'],
                 sources = ['deepin_gsettings_python.c'])
 
-setup(name='deepin-gsettings',
+setup(name='deepin_gsettings',
       version='0.1',
       ext_modules = [deepin_gsettings_mod],
-      description='Deepin Gsettings Python binding.',
-      long_description ="""Deepin Gsettings Python binding.""",
+      description='Deepin GSettings Python binding.',
+      long_description ="""Deepin GSettings Python binding.""",
       author='Linux Deepin Team',
       author_email='zhaixiang@linuxdeepin.com',
       license='GPL-3',
@@ -25,4 +25,3 @@ setup(name='deepin-gsettings',
       download_url="git@github.com:linuxdeepin/deepin-gsettings.git",
       platforms = ['Linux'],
       )
-
